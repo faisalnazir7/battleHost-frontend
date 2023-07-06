e32f8a556bd158f13308ae75e5df0623ed596bd2
Ritika Saxena <ritikasaxena911@gmail.com>
2023-07-06 11:49:04 +0530

    added the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
 
 function App() {
 
   return (
     <>
-      <div>
-      <h1 className="text-xl font-bold">
-      Hello world!
-    </h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     {/* <Flowbite> */}
+     {/* <DarkThemeToggle /> */}
+      <Navbar/>
+      
+      {/* </Flowbite> */}
     </>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <div className='Navbar'>
+    <div className='Navbar h-20 bg-sky-800'>
         <div className="Search">
             <input type="text" placeholder='Search...' />
         </div>
-        <button>Log In</button>
-        <button>Register</button>
+        <button className='rounded-2xl border border-slate-300 hover:border-slate-400 h-12 w-20 bg-white ml-auto'>Log In</button>
+        <button className='rounded-2xl border border-slate-300 hover:border-slate-400 h-12 w-20 bg-white items-end mr-5'>Register</button>
     </div>
   )
 }
