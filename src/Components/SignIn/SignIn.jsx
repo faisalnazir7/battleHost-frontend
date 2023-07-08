8c91e725d98ecbb27bdcc8eafb4f7f1d8764209c
Ritika Saxena <ritikasaxena911@gmail.com>
2023-07-08 10:54:48 +0530

    added the signup page

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function SignIn() {
   return (
-    <div className='SignIn w-96 h-96 bg-slate-200 m-auto mt-20'>
+    <div className='SignIn w-96 h-[26rem] bg-slate-200 m-auto mt-20'>
         <h1 className='text-center text-2xl pt-4'>Sign in</h1>
         <p className='text-center pt-4'>Welcome back! Please enter your details</p>
         <div className="SignInForm ml-12  pt-6">
@@ -13,7 +13,7 @@ export default function SignIn() {
         <input type="text" className='pl-4 pr-4 w-72 h-10 rounded-2xl outline-none' />
         </div>
         <button className='bg-sky-800 text-white w-80 h-12 mt-8 ml-8  rounded-full'>Sign In</button>
-        <p className='pt-4 text-center'>New User? <Link to='/signup' className='text-blue-600'>Register</Link> </p>
+        <p className='pt-4 text-center'>New User? <Link to='/signup' className='text-blue-600 underline'>Register</Link> </p>
     </div>
   )
 }
