1bbe9e609c197e3165b240095c3ff9bb8947bb01
Ritika Saxena <ritikasaxena911@gmail.com>
2023-07-08 13:02:24 +0530

    added the signin signup page responsive

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div className='SignUp w-96 h-[30rem] bg-slate-200 m-auto mt-20'>
+    <div className='SignUp w-96 h-[30rem] bg-slate-200 m-auto mt-20 md:w-[19rem]'>
         <h1 className='text-center text-2xl pt-4'>Sign Up</h1>
         <p className='text-center pt-4'>Get started by signing up!</p>
-        <div className="SignUpForm ml-12  pt-6">
+        <div className="SignUpForm ml-12  pt-6 md:ml-[0.5rem]">
             <h2>Your UserName</h2>
             <input type="text" className='pl-4 pr-4 w-72 h-10 rounded-2xl outline-none' />
             <h2 className='pt-4'>Your Email</h2>
@@ -14,7 +14,7 @@ export default function SignUp() {
             <h2 className='pt-4'>Your Password</h2>
             <input type="text" className='pl-4 pr-4 w-72 h-10 rounded-2xl outline-none' />
         </div>
-        <button className='bg-sky-800 text-white w-80 h-12 mt-8 ml-8  rounded-full'>Sign Up</button>
+        <button className='bg-sky-800 text-white w-80 h-12 mt-8 ml-8  rounded-full md:ml-4 md:w-[17rem]'>Sign Up</button>
         <p className='pt-4 text-center'>Already Registered? <Link to='/signin' className='text-blue-600 underline'>Log In</Link> </p>
     </div>
   )
