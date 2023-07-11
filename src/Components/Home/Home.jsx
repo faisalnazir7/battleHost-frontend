5a3d33ccc5f1c1d28314a6b5536736020c3ec4a2
Ritika Saxena <ritikasaxena911@gmail.com>
2023-07-11 15:02:22 +0530

    made the carousel section file

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,34 +1,13 @@
-
 import React from 'react';
-import { Carousel } from 'flowbite-react';
 import Navbar from '../Navbar/Navbar';
+import Carousel from '../Carousel/Carousel';
 
-export default function DefaultCarousel() {
+
+export default function Home() {
   return (
     <>
     <Navbar/>
-    {/* <Carousel>
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-      />
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-      />
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-      />
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-      />
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-      />
-    </Carousel> */}
+   <Carousel/>
     </>
   )
 }
