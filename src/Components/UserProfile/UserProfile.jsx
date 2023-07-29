f18e9d4933ac7c650fd4e30fc3f8e02cd308b183
Swarga-codes <sd3526@srmist.edu.in>
2023-07-30 00:00:05 +0530

    removed unnecessary comment Co-authored-by: Swarga-codes sd3526@srmist.edu.in Co-authored-by: faisalnazir7 bhatfaisalfnb@gmail.com

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -45,10 +45,6 @@ function UserProfile() {
     <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
     <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required disabled/>
 </div> 
-{/*<div className="mb-6">
-    <label for="confirm_password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-    <input type="password" id="confirm_password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required/>
-  </div> */}
 <div className="flex items-start mb-6">
    
     <label for="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">The updated changes will take effect immediately on clicking the button below.</label>
