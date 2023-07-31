817085a40730f7bb0e710b26f87e7c229c9b7f38
Prajjawal Sharma <78965341+Prajjawal-Sharma@users.noreply.github.com>
2023-07-31 23:35:30 +0530

    minor changes (#28)
    * added profile page
    
    * minor changes in profile page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Routes, Route} from 'react-router-dom'
 import SignIn from './Components/SignIn/SignIn';
 import SignUp from './Components/SignUp/SignUp';
 import Dasboard from './Components/Dasboard/Dasboard';
+import Profile from './Components/Profile/Profile'
 import UserProfile from './Components/UserProfile/UserProfile';
 import Profile from './Components/Profile/Profile';
 
