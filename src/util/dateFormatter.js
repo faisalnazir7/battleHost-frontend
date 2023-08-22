325bde6e2960f8f8302b24cd396f7a5e915821a8
Swarga-codes <sd3526@srmist.edu.in>
2023-08-22 22:01:57 +0530

    displayed all tournaments

---
+++ src/util/dateFormatter.js
@@ -0,0 +1,18 @@
+export default function dateFormatter(inputDate) {
+    const date = new Date(inputDate);
+    date.setDate(date.getDate() - 1); // Subtract one day
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = date.toLocaleString('default', { month: 'short' });
+    const year = String(date.getFullYear()).slice(-2);
+    
+    const timeOptions = {
+      hour: '2-digit',
+      minute: '2-digit',
+      timeZone: 'Asia/Kolkata', // Indian Standard Time (IST)
+    };
+    const timeInIST = date.toLocaleTimeString('en-IN', timeOptions);
+  
+    return `${day}-${month}-${year}, ${timeInIST} IST`;
+  }
+  
+  
