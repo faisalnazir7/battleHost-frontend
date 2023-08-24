6039813236a6652cace4ca1ba52dd98c2c40b32e
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-08-24 23:39:28 +0530

    added components

--- src/Pages/Result/Result.jsx
+++ src/Pages/Result/Result.jsx
@@ -5,24 +5,28 @@ import silver from '../../assets/silver trophy.png'
 import bronze from '../../assets/bronze trophy.png'
 import {useWindowSize} from 'react-use';
 import Confetti from 'react-confetti'
+
+
 export default function Result() {
     const { width, height } = useWindowSize()
   return (
     <>
     <Navbar/>
+    
     <Confetti
       width={width}
       height={height}
       recycle={false}
       numberOfPieces={500}
     />
-    <div className='Result flex m-auto w-1/2'>
-    <div className="second mt-44">
+    <div className="Result flex m-auto w-1/2">
+
+    <div className="second mt-40">
             <img src={silver} alt=""  className='w-52'/>
             <div className="resContent text-center text-xl bg-gray-300 h-32 -mt-6">
                 <h1 className='pt-4'>Team: Error-404</h1>
                 <h2>Leader:XYZ</h2>
-                <h3>Rs 50000</h3>
+                <h3>Rs 100000</h3>
             </div>
         </div>
         <div className="first ">
@@ -38,7 +42,7 @@ export default function Result() {
             <div className="resContent text-center text-xl bg-[#bc752f] h-32 -mt-7">
                 <h1 className='pt-4'>Team: Error-404</h1>
                 <h2>Leader:XYZ</h2>
-                <h3>Rs 50000</h3>
+                <h3>Rs 10000</h3>
             </div>
         </div>
     </div>
