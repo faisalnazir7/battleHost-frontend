19d3b580f138299c1c3698db6e627af784714e53
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-09-04 00:02:46 +0530

    Fix: Crash

--- src/Pages/TournamentManage/TournamentManage.jsx
+++ src/Pages/TournamentManage/TournamentManage.jsx
@@ -1,15 +1,5 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-
-function TournamentManage() {
-    const {tournamentId}=useParams()
-    
-  return (
-    <div>TournamentManage {tournamentId}</div>
-  )
-}
-
-export default TournamentManage
 import SideNav from '../../Components/SideNav/SideNav'
 import Navbar from '../../Components/Navbar/Navbar'
 
