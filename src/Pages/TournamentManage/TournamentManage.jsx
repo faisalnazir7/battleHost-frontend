680e84a9943d738fc56a49cb6af166537b95dc2e
Ritika Saxena <ritikasaxena911@gmail.com>
2023-09-04 03:18:05 +0530

    added the manage card

--- src/Pages/TournamentManage/TournamentManage.jsx
+++ src/Pages/TournamentManage/TournamentManage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import SideNav from '../../Components/SideNav/SideNav'
 import Navbar from '../../Components/Navbar/Navbar'
+import ManageCard from '../../Components/ManageCard/ManageCard'
 
 export default function TournamentManage() {
   return (
@@ -9,7 +10,7 @@ export default function TournamentManage() {
       <Navbar />
       <div className='TournamentManage flex'>
         <SideNav />
-        <div className="TournamentManageContent ml-12 mt-6">
+        <div className="TournamentManageContent ml-12 mt-14">
 
           <div className='HeadingTournament border-b'>
             <h2 className="text-xl font-semibold">Manage Your Tournaments</h2>
@@ -20,28 +21,8 @@ export default function TournamentManage() {
           </div>
 
           <div >
-            <table className='w-full border-separate border-spacing-y-20'>
-              <tr className='border-b'>
-                <th className='pr-60 text-left'>Title</th>
-                <td className='pr-96 '>HackRx 2.0</td>
-               <button className='pr-96 text-blue-700 '>Update</button>
-              </tr>
-              <tr >
-              <th className='pr-60 text-left '>Title</th>
-                <td className='pr-96 '>HackRx 2.0</td>
-               <button className='pr-96 text-blue-700 '>Update</button>
-              </tr>
-              <tr >
-              <th className='pr-60 text-left '>Title</th>
-                <td className='pr-96 '>HackRx 2.0</td>
-               <button className='pr-96 text-blue-700 '>Update</button>
-              </tr>
-              <tr >
-              <th className='pr-60 text-left '>Title</th>
-                <td className='pr-96 '>HackRx 2.0</td>
-               <button className='pr-96 text-blue-700 '>Update</button>
-              </tr>
-            </table>
+            
+            <ManageCard/>
           </div>
         </div>
       </div>
